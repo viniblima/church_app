84650b544c2fb51e0070ad255299b19ae6335146
Vinícius Branco <viniblima2016@gmail.com>
2022-12-23 17:33:12 -0300

    feat: :sparkles: inicia o projeto

---
+++ lib/widgets/post_card_vertical.widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PostCardVertical extends StatelessWidget {
+  const PostCardVertical({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
